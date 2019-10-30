4eb7fbe92499e3a5995911f734c3d66f05f3f1d7
Roman Zillinger <r.zillinger@zoovu.com>
2019-10-30 21:46:21 +0100

    options page shows some time tracks

--- src/app/options/options.tsx
+++ src/app/options/options.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { WorkTrackStore } from "../WorkTrackStorage";
+import { WorkTrackDayRecord } from "../WorkTrackDayRecord";
 
 const MonthlyOverview: React.FunctionComponent<{}> = ({}) => {
+  const store = new WorkTrackStore(chrome.storage.local, chrome.runtime);
+  const now = new Date(Date.now());
+
+  const [records, setRecords] = useState<WorkTrackDayRecord[]>([]);
+
+  store.getRecordsOfMonth(
+    now.getFullYear(),
+    now.getMonth() + 1,
+    data => {
+      if (records.length === 0) {
+        setRecords(data);
+      }
+    },
+    err => alert("error happened: " + err)
+  );
+
   return (
     <>
       <h2>here should records of month show up</h2>
+      <table>
+        <thead>
+          <th>day</th>
+          <th>records</th>
+        </thead>
+        <tbody>
+          {records.map(rec => {
+            return (
+              <>
+                <tr>
+                  <td>{rec.date}</td>
+                  <td>
+                    {rec.records.map(r => {
+                      return (
+                        <>
+                          <span>
+                            {r.start}:{r.end ? r.end : ""}
+                          </span>
+                        </>
+                      );
+                    })}
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
